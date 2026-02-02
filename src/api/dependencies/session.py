31b5e104ee4592a4807eab4cee0a791274f07f5c
Mudit Gupta <muditgupt01@gmail.com>
2026-02-02 11:40:41 +0530

    refactoring

--- src/api/dependencies/session.py
+++ src/api/dependencies/session.py
@@ -1,14 +1,11 @@
 from __future__ import annotations
 
-from typing import Generator
-
 from pymongo.collection import Collection
 
 from src.models.mongodb import get_prompt_requests_collection
 
 
-def get_db_collection() -> Generator[Collection, None, None]:
+def get_db_collection() -> Collection:
     """Get MongoDB collection for prompt_requests."""
-    collection = get_prompt_requests_collection()
-    yield collection
+    return get_prompt_requests_collection()
 
